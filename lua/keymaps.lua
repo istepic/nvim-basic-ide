4f13b011fe8ac113b6981863ae0c7d25bc18ed79
istepic <ivan.stepich@gmail.com>
2022-12-12 23:33:24 +0100

    do i need gitsigns with lazygit?

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -29,16 +29,16 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
-
--- Clear highlights
-keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
+keymap("n", "<leader>w>", ":bnext<CR>", opts)
+keymap("n", "<leader>q>", ":bprevious<CR>", opts)
 
 -- Close buffers
 keymap("n", "<S-q>", "<cmd>Bdelete!<CR>", opts)
 
--- Better paste
+-- Clear highlights
+keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
+
+-- Better paste in visual mode
 keymap("v", "p", '"_dP', opts)
 
 -- Insert --
