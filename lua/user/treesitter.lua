098abd8184791ea85273be2902eb6681872d8852
istepic <ivan.stepich@gmail.com>
2023-04-29 16:02:35 +0200

    bracket fix

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -16,7 +16,6 @@ local M = {
   },
 }
 function M.config()
-  local treesitter = require "nvim-treesitter"
   local configs = require "nvim-treesitter.configs"
 
 configs.setup({
@@ -24,7 +23,7 @@ configs.setup({
   -- ensure_installed = "all", -- one of "all" or a list of languages
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  
+
   highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { "css" }, -- list of language that will be disabled
@@ -38,7 +37,7 @@ configs.setup({
       enable = true,
       enable_autocmd = false,
     },
-  }
+  })
 end
 
 return M
